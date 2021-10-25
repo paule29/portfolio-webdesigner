938182f0fc7217d053bbaaad259e1da139337f31
Paule29 <pauleombiono@gmail.com>
2021-10-25 10:16:02 +0200

    portfolio with contact form

---
+++ handler.php
@@ -0,0 +1,20 @@
+<?php
+
+if(isset($_POST['user_mail'])&& !empty($_POST['user_mail'])){
+    $email = $_POST['user_mail'];
+    $objet = $_POST['message_topic'];
+    $message = $_POST['user_message'];
+    $recipient = 'p.ombiono@codeur.online';
+    $send = 'message envoyé par '.$email.' : '.$message;
+    $headerFields = array(
+      "From: {$email}",
+      "MIME-Version: 1.0",
+      "Content-Type: text/html;charset=utf-8"
+      );
+    mail($recipient, $objet, $send, implode("\r\n", $headerFields));
+    echo "message envoyé <br>";
+    echo "<a href='index.php'>Retour</a>";
+}
+else{
+  echo 'erreur';
+}
---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Personal Portfolio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+  </div>
+    <!-- début de la page -->
+    <header>
+    <div class="user">
+    <img src="images/paule.jpg" alt="">
+    <h3 class="name">Paule OMBIONO</h3>
+    <p class="poste">Web Designer </p>
+     
+    </div>
+
+    <nav class="navbar">
+        <ul>
+        <li><a href="#home">Accueil</a></li>
+        <li><a href="#about">A propos</a></li>
+        <li><a href="#experience">Experiences</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contacts</a></li>
+        
+        </ul>
+    </nav>
+
+    </header>
+
+    <div id="menu" class="fas fa-bars"></div>
+
+    <!--home section début -->
+<section class="home" id="home">
+    <h3>Bonjour !</h3>
+    <h1>je suis <span>Paule OMBIONO</span></h1>
+    <p> Stagiaire de la formation professionnelle depuis mars 2021 en reconversion professionnelle, passionnée par les métiers du numériques, je voudrais faire de cette passion un métier. Sachat que l'illétré de demain ne sera pas celui qui ne sera pas allé à l'école mais celui qui ne saura pas se servir de l'outil informatique. 
+    </p>
+    <a href="#about"><button class="btn">A propos de moi <i class="fas fa-user"></i></button></a>
+
+
+</section>
+ <!--home section fin-->
+<!-- début section about-->
+<section class="about" id="about">
+    <h1 class="heading"> <span>A propos</span> de moi </h1>
+    <div class="row">
+        <div class="info">
+            <h3> <span> nom :</span> Paule OMBIONO </h3>
+            <h3> <span> age :</span> 38 ans</h3>
+            <h3> <span> poste :</span> Web Designer </h3>
+            <h3> <span> langue :</span> Français </h3>
+            <a href="#"><button class="btn">Téléchargez CV <i class="fas fa-download"></i> </button></a>
+
+        </div>
+        <div class="counter">
+            <div class="box">
+                <span>- de 1 an </span>
+                <h3>expériences</h3>
+            </div>
+            
+            <div class="box">
+                <span>3</span>
+                <h3>projets completés</h3>
+            </div>
+            
+            
+        </div>
+    </div>
+
+</section>
+
+<!-- fin section about-->
+
+<!-- début section experience -->
+
+<section class="experience" id="experience">
+<h1 class="heading"> Mes <span>Experiences</span> </h1>
+<div class="box-container">
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>2021</span>
+        <h3>Web Designer</h3>
+        <p>Apprendre à coder est intimidant, alors préparez-vous à réussir avec un outil conçu pour vous. Visual Studio Code est un éditeur de codage gratuit qui vous aide à commencer à coder rapidement.</p>
+
+    </div>
+
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>2019 - 2020</span>
+        <h3>Assistante de télécommunication / support téléphonique DOCAPOST</h3>
+        <p>Bureau d'Etude ENEO INFRACOM (DIJON);
+        <P>Technicienne D1, D2, D3 chez STELSEO (DORDOGNE);
+        <p>
+        </p>
+
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>2019</span>
+        <h3>Technicienne Réseaux télécoms</h3>
+        <p>Bureau d'Etude ENEO INFRACOM (DIJON);
+        <P>Technicienne D1, D2, D3 chez STELSEO (DORDOGNE);
+        <p>
+        </p>
+
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>2018</span>
+        <h3>Technicienne reseaux télécoms</h3>
+        <p>Apprendre à coder est intimidant, alors préparez-vous à réussir avec un outil conçu pour vous. Visual Studio Code est un éditeur de codage gratuit qui vous aide à commencer à coder rapidement.</p>
+
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>2017</span>
+        <h3>Technicienne reseaux télécoms</h3>
+        <p>Apprendre à coder est intimidant, alors préparez-vous à réussir avec un outil conçu pour vous. Visual Studio Code est un éditeur de codage gratuit qui vous aide à commencer à coder rapidement.</p>
+
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>2016</span>
+        <h3>Technicienne reseaux télécoms</h3>
+        <p>Apprendre à coder est intimidant, alors préparez-vous à réussir avec un outil conçu pour vous. Visual Studio Code est un éditeur de codage gratuit qui vous aide à commencer à coder rapidement.</p>
+        
+    </div>
+
+</div>
+
+</section>
+
+<!-- fin section experience -->
+
+<!-- début section portfolio -->
+
+<section class="portfolio" id="portfolio">
+    <h1 class="heading"> Mon <span>Portfolio</span> </h1>
+    <div class="box-container">
+        <div class="box">
+            <img src="images/paule1.jpg" alt="" alt="">
+        </div>
+        <div class="box">
+            <img src="images/paule1.jpg" alt="" alt="">
+        </div>
+        <div class="box">
+            <img src="images/photo2.jpeg" alt="" alt="">
+        </div>
+        <div class="box">
+            <img src="images/paule1.jpg" alt="" alt="">
+        </div>
+        <div class="box">
+            <img src="images/paule1.jpg" alt="" alt="">
+        </div>
+        <div class="box">
+            <img src="images/paule1.jpg" alt="" alt="">
+        </div>
+    </div>
+
+</section>
+<!-- fin section portfolio -->
+
+<!-- début section contact -->
+
+<section class="contact" id="contact">
+    <h1 class="heading"> Me <span>Contacter</span> </h1>
+    <div class="row">
+        <div class="content">
+            <h3 class="title">Contact Info</h3>
+            <div class="info">
+                <h3> <i class="fas fa-envelope"></i>p.ombiono@codeur.online</h3>
+                <h3> <i class="fas fa-phone"></i>07 83 35 65 50</h3>
+                <h3> <i class="fas fa-map-marker-alt"></i>Cote d'Or, 21000 Dijon France</h3>
+            </div>
+
+        </div>
+        <form action="handler.php" method="POST">
+            <input type="text" placeholder="votre nom" class="box">
+            <input type="email" placeholder="votre email" class="box" name='user_mail'>
+            <input type="text" placeholder="projet" class="box" name='message_topic'>
+            <textarea name="user_message" cols="30" rows="10" class="box message" placeholder="votre message"></textarea>
+            <button type="submit" class="btn"> Envoyer <i class="fas fa-paper-plane"></i></button>
+        </form>
+    </div>
+
+</section>
+<!-- fin section contact -->
+
+<!-- barre de navigation -->
+<div>
+        <button onclick ="topFunction()" id="myBtn" title="Go to top">Haut de page
+        </button>
+</div>
+
+
+
+
+
+
+
+
+    
+    <script type="" src="main.js"></script>
+
+</body>
+</html>
